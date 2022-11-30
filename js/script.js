ee1b549d51f2e3e841603759ba306634a2ad0485
RenatoFringuello <renatofringuello@hotmail.it>
2022-11-30 15:57:00 +0100

    add function to add element

---
+++ js/script.js
@@ -0,0 +1,14 @@
+function addElement(tagName, className){
+    const element = document.createElement(tagName);
+    element.classList.add(className);
+    return element;
+}
+
+const game = document.getElementById('game');
+const gameField = addElement('div', 'd-flex');
+
+game.append(gameField);
+
+
+
+
