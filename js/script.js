1321cd52e80441a2ee40016921d220fc3532a915
RenatoFringuello <renatofringuello@hotmail.it>
2022-11-30 16:03:48 +0100

    add corrections in function createElement

--- js/script.js
+++ js/script.js
@@ -1,14 +1,15 @@
-function createElement(tagName, className){
+function createElement(tagName, className = ''){
     const element = document.createElement(tagName);
-    element.classList.add(className);
+    element.className = className;
     return element;
 }
 
 const game = document.getElementById('game');
-const gameField = addElement('div', 'd-flex');
-
+const gameField = createElement('div', 'd-flex ciao');
 game.append(gameField);
 
-
+// for (let i = 0; i < 10; i++) {
+//     const cell = createElement('div', 'd-flex');
+// }
 
 
